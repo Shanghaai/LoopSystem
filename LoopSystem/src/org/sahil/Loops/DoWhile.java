cb9aeca14fc949bd73e1fe667a221230770f5f96
Shanghaai <34318533+Shanghaai@users.noreply.github.com>
2017-12-11 18:06:36 +0530

    Add files via upload

---
+++ LoopSystem/src/org/sahil/Loops/DoWhile.java
@@ -0,0 +1,18 @@
+package org.sahil.Loops;
+
+public class DoWhile {
+public static void main(String[] args) 
+{
+	int x=0;
+	do
+	{
+		x++;
+		System.out.println(x);
+		if(++x<5)
+			continue;
+		x++;
+		System.out.println(x);
+	}
+	while(++x<10);
+ }
+}
